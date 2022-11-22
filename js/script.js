ffd0de7023defe253143c5cdaa22cab0da8273da
Andrea-Baggio <andreabaggio800@gmail.com>
2022-11-22 15:58:47 +0100

    console.log

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+new Vue ({
+    el: '#app',
+    data: {
+        email: '',
+    },
+    methods: {
+        getEmail () {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(myEmail => {
+                console.log(myEmail)
+            })
+        }
+    }
+})
